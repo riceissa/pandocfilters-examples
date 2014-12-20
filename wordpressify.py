3bbdb695be3c72892b93896c2dacee8ed86124f5
Issa Rice <riceissa@gmail.com>
2014-12-19 18:22:03 -0800

    write wordpressify

---
+++ wordpressify.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# This is a Python (using pandocfilters) implementation of wordpressify.hs
+# from http://johnmacfarlane.net/pandoc/scripting.html#latex-for-wordpress
+
+from pandocfilters import toJSONFilter, Math
+
+def wordpressify(key, value, format_, meta):
+    '''
+    Convert math formulas so they work with WordPress.
+    '''
+    if key == 'Math':
+        return Math(value[0], "LaTeX " + value[1])
+
+if __name__ == '__main__':
+    toJSONFilter(wordpressify)
