6c6212bae029b8d77b5999b7cb76bb070ead5317
Issa Rice <riceissa@gmail.com>
2014-12-19 19:30:34 -0800

    write delink

---
+++ delink.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# This is a Python (using pandocfilters) implementation of delink.hs
+# from http://johnmacfarlane.net/pandoc/scripting.html#removing-links
+
+from pandocfilters import toJSONFilter, Para, Emph
+
+def delink(key, value, format_, meta):
+    '''
+    Remove links but retain their text.
+    '''
+    if key == 'Link':
+        txt, link = value
+        return txt
+
+if __name__ == '__main__':
+    toJSONFilter(delink)
