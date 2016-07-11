88226e46a281cdf72ef8876704f18c4af70ec897
Issa Rice <riceissa@gmail.com>
2016-07-11 10:04:12 -0700

    fix for pandoc 1.17

--- extracturls.py
+++ extracturls.py
@@ -2,7 +2,7 @@
 
 # This is a Python (using pandocfilters) implementation of
 # extracturls.hs from
-# http://johnmacfarlane.net/pandoc/scripting.html#queries-listing-urls
+# http://pandoc.org/scripting.html#queries-listing-urls
 
 from pandocfilters import walk, Para, Emph
 import sys, json
@@ -10,13 +10,13 @@ import sys, json
 def extracturls(key, value, format_, meta):
     '''
     "[E]xtract all the URLs linked to in a markdown document" (
-    http://johnmacfarlane.net/pandoc/scripting.html#queries-listing-urls
-    )
+    http://pandoc.org/scripting.html#queries-listing-urls )
     '''
-    if key == 'Link' or key == 'Image':
-        print value[1][0]
+    if key == "Link" or key == "Image":
+        _, _, (url, _) = value
+        print(url)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     doc = json.loads(sys.stdin.read())
     if len(sys.argv) > 1:
         format_ = sys.argv[1]
